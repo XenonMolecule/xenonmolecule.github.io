410397239af8058aa09d1cf0100bf344e501b7fd
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-01-16 02:22:38 +0000

    Added tabs, subtitle, header, and all that jazz

---
+++ main.js
@@ -0,0 +1,7 @@
+// JavaScript File
+
+$(".header ul .textLinks a li").on("mouseenter",function(){
+    $(this).addClass("hover");
+}).on("mouseleave",function(){
+    $(this).removeClass("hover");
+})
