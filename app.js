350635b9c113f3b9e3bfaa4e95cfeec7c02e29a1
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-29 23:04:44 +0000

    added color selector for adventerous folks

--- app.js
+++ app.js
@@ -128,17 +128,20 @@ $(".exitPaint").click(function(e){
 });
 
 //switch the selected color
-$(".colorPallet ul li").each(function(){
-   $(this).click(function(){
-       $(".colorPallet ul li").each(function(){
-            if($(this).hasClass("selectedColor")){
-                $(this).removeClass("selectedColor");
-            }
+function initPallet(){
+    $(".colorPallet ul li").each(function(){
+       $(this).click(function(){
+           $(".colorPallet ul li").each(function(){
+                if($(this).hasClass("selectedColor")){
+                    $(this).removeClass("selectedColor");
+                }
+           });
+           $(this).addClass("selectedColor");
+           color = $(".selectedColor").css("background-color");
        });
-       $(this).addClass("selectedColor");
-       color = $(".selectedColor").css("background-color");
-   });
-});
+    });
+}
+initPallet();
 
 //enable drawing on the canvas
 $("#paintCanvas").mousedown(function(e){
@@ -161,26 +164,91 @@ $("#paintCanvas").mousedown(function(e){
 //Solution: Add functionality to the + button and color selection
 
 //make the selector appear and disappear
-$(".addColor").click(function(){
-    addColorOffset=38;
-    $(".colorPallet").children().each(function(){
-        addColorOffset+=55;
+function initColorAdder(){
+    $(".addColor").on("click",function(){
+        addColorOffset=38;
+        $(".colorPallet").children().each(function(){
+            addColorOffset+=55;
+        });
+        addColorOffset-=150;
+        $(".colorSelector").css("margin-left",addColorOffset);
+        if(($(".colorSelector").prop("hidden"))){
+            $(".colorSelector").attr("hidden",false);
+        } else{
+            $(".colorSelector").attr("hidden",true);
+        }
     });
-    addColorOffset-=150;
-    $(".colorSelector").css("margin-left",addColorOffset);
-    if(($(".colorSelector").prop("hidden"))){
-        $(".colorSelector").attr("hidden",false);
-    } else{
-        $(".colorSelector").attr("hidden",true);
-    }
-});
+}
+initColorAdder();
 
+var mouseDownElement="";
 //make the color display what is shown by the sliders
-$(".colorSlider").each(function(){
-   $(this).on("change",function(){
-       //code from HERE now
-   });
-});
+function initColorDisplay(){
+    $(".colorSlider").each(function(){
+        $(this).on("mousedown",function(){
+            mouseDownElement = this;
+            setTimeout(adjustColor,10);
+        })
+        $(this).on("mouseup",function() {
+            if(mouseDownElement===this){
+                mouseDownElement="";
+            }
+            setTimeout(adjustColor,10);
+        })
+        $(this).on("mousemove",function(){
+            if(mouseDownElement===this){
+                setTimeout(adjustColor,1);
+            }
+       });
+    });
+}
+initColorDisplay();
+
+function adjustColor(){
+    $(".displayColor").css("background-color","rgb("+$("#red").val()+","+$("#green").val()+","+$("#blue").val()+")");
+}
 
 
 //append the color
+var colorCount=0;
+var colorLimit=Math.floor((window.innerWidth-240)/60);
+var reset = $(".backupPallet").html();
+function initColorAppender(){
+    
+    $("#appendColor").on("click",function(){
+        code : {
+            colorCount++;
+            var newColorLimit = Math.floor((window.innerWidth-240)/60);
+            if(colorLimit!=newColorLimit){
+                colorCount = 0;
+                colorLimit=Math.floor((window.innerWidth-240)/60);
+                $(".colorPallet").empty().append(reset);
+                color = $(".selectedColor").css("background-color");
+                $(".exitPaint").click(function(e){
+                    e.preventDefault();
+                    $(".colorPallet").attr("hidden",true);
+                    $("#paintCanvas").attr("hidden",true);
+                    paintContext.clearRect(0,0,width,height);
+                });
+                $(".exitPaint").css("margin-left",(window.innerWidth-55));
+                initColorAdder();
+                initColorDisplay();
+                initColorAppender();
+                break code;
+            }
+            colorLimit=Math.floor((window.innerWidth-240)/60);
+            var element = $("<li class='color"+colorCount+"'></li>")
+            element.css("backgroundColor","rgb("+$("#red").val()+","+$("#green").val()+","+$("#blue").val()+")");
+            if(colorCount<=colorLimit){
+                $(".addColor").before(element);
+                $(".addColor").css("margin-left",(240+(55*(colorCount-1))+"px"));
+            } else {
+                console.log(".color"+(colorCount-colorLimit));
+                $(".color"+(colorCount-colorLimit)).attr("class","color"+colorCount).css("backgroundColor","rgb("+$("#red").val()+","+$("#green").val()+","+$("#blue").val()+")");
+            }
+            $(".colorSelector").attr("hidden",true);
+            initPallet();
+        }
+    });
+}
+initColorAppender();
