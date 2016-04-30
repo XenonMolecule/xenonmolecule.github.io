0216b4e393771cbd4a1db2b0a76433fbdd6e8b86
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-30 01:36:33 +0000

    added projects to the projects page (finally)

---
+++ tabs/projects/app.js
@@ -0,0 +1,9 @@
+// JavaScript File
+
+var dataRequest = $.getJSON("projects.json").done(function(data){
+    for(var label in data){
+        var creation = '<div id="project"><img src="'+data[label].thumbnail+'"><a href="'+data[label].link+'"><p>'+label+'</p></a><p id="description">'+data[label].description+'</p></div>'
+        creation = $(creation);
+        $(".projects").append(creation);
+    }
+})
