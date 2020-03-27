7c0f744c0e616f7d6ad64d7606b0ad42d560d41f
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-03-27 19:52:23 -0400

    Switched to gatsby

--- main.js
+++
@@ -1,8 +0,0 @@
-// JavaScript File
-
-$(".header ul .textLinks a li").on("mouseenter",function(){
-    $(this).addClass("hover");
-}).on("mouseleave",function(){
-    $(this).removeClass("hover");
-})
-
