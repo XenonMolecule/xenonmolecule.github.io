77bf62acdf4d4a42ea53b45075feee780e13dd91
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-03-28 00:18:07 -0400

    Added dope particle background

---
+++ src/components/particlecanvas.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Particles from 'react-particles-js';
+
+class ParticleCanvas extends React.Component {
+
+  render() {
+    return (
+        <Particles className={'background'}/>
+    );
+  }
+
+}
+
+export default ParticleCanvas;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components/main.css'
+import ParticleCanvas from "../components/particlecanvas"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
+  <>
+    <Layout>
+      <SEO title="Home" />
+    </Layout>
+    <ParticleCanvas/>
+  </>
 )
 
 export default IndexPage
