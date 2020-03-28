b522954be6773267460eb4bb2a2a3fdcb03b65d3
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-03-28 01:22:28 -0400

    Added in selfie

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,25 @@ import SEO from "../components/seo"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components/main.css'
 import ParticleCanvas from "../components/particlecanvas"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import SelfieImg from "../components/selfieimg"
 
 const IndexPage = () => (
   <>
     <Layout>
       <SEO title="Home" />
+      <Container>
+        <Row>
+          <Col>
+            <h1>MICHAEL RYAN</h1>
+          </Col>
+          <Col>
+            <SelfieImg/>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
     <ParticleCanvas/>
   </>
